f868acc563324a81730a51db5887d26881c535cb
selva.g <selva.g@skitter.in>
2022-09-30 12:32:29 +0530

    [initial commit]

---
+++ src/main.rs
@@ -0,0 +1,205 @@
+// use axum::Router;
+
+use std::{
+    collections::HashMap,
+    error::Error,
+    fmt,
+    sync::{Arc, RwLock},
+};
+
+use axum::{
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Router, Server as A_Server,
+};
+use error_stack_derive::ErrorStack;
+use rand::prelude::*;
+use tokio::{self};
+use uuid::Uuid;
+
+// use error_stack::{Context, IntoReport, Report, Result, ResultExt};
+
+use error_stack::{IntoReport, Report, ResultExt};
+
+#[derive(ErrorStack, Debug, Default)]
+#[error_message(&format!("Error occured with Guess2 "))]
+struct GuessError2;
+
+#[derive(ErrorStack, Debug, Default)]
+#[error_message(&format!("Error occured with foo "))]
+struct GuessError;
+
+#[derive(Debug, ErrorStack)]
+enum CustomErrors {
+    #[error_message(&format!("Error occured with foo "))]
+    InvalidGame { game_id : String},
+    #[error_message(&format!("Error occured with foo "))]
+    GuessDataMissingForGame { game_id: String },
+}
+
+fn infer_lifetime<'a, T: 'a, F: Fn(&'a T) -> &'a T>(f: F) -> F {
+    f
+}
+
+// the application state
+#[derive(Clone)]
+struct AppState {
+    // that holds some api specific state
+    number_to_guess: u32,
+}
+
+#[derive(Clone, Default)]
+struct AppDBState {
+    guess_pairs: HashMap<String, u32>,
+}
+
+type SharedAppDBState = Arc<RwLock<AppDBState>>;
+
+struct AppError(GuessError2);
+
+impl IntoResponse for GuessError2 {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self),
+        )
+            .into_response()
+    }
+}
+
+impl From<GuessError2> for AppError {
+    fn from(g: GuessError2) -> Self {
+        AppError(g)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    let shared_state: AppState = AppState {
+        number_to_guess: 10,
+    };
+
+    let shared_db_state: SharedAppDBState = SharedAppDBState::default();
+
+    // build our application with a route
+    let app = Router::new()
+        .route(
+            "/",
+            get(|| async {
+                // println!("Service alive");
+                format!("Service alive")
+            }),
+        )
+        .route("/generate-game-id", get(generate_game_handler))
+        .route("/reveal/:id", get(reveal_handler))
+        .route("/guess/:id/:guess", get(guess_num_handler))
+        .layer(Extension(shared_db_state))
+        .into_make_service();
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    // tracing::debug!("listening on {}", addr);
+    A_Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app)
+        .await
+        .unwrap();
+}
+
+async fn generate_game_handler(Extension(state): Extension<SharedAppDBState>) -> String {
+    let id = Uuid::new_v4();
+    let mut rng = rand::thread_rng();
+    let gen_num: u32 = rng.gen_range(0..20);
+    state
+        .write()
+        .unwrap()
+        .guess_pairs
+        .insert(id.to_string(), gen_num);
+    id.to_string()
+}
+
+async fn reveal_handler(
+    Path(id): Path<String>,
+    Extension(state): Extension<SharedAppDBState>,
+) -> Result<String, GuessError2> {
+    let y = state
+        .read()
+        .map_err(|s| GuessError2)
+        .report()
+        .change_context(GuessError)?;
+
+    let y = y.guess_pairs.get(&id).ok_or_else(|| GuessError).report()?;
+
+    Ok(y.to_string())
+}
+
+async fn guess_num_handler(
+    Path((id, guess)): Path<(String, u32)>,
+    // Path(guess): Path<u32>,
+    Extension(state): Extension<SharedAppDBState>,
+) -> Result<String, GuessError2> {
+    let y = state
+        .read()
+        .map_err(|s| GuessError2)
+        .report()
+        .change_context(GuessError)?;
+
+    let y1 = y.guess_pairs.get(&id).ok_or_else(|| GuessError).report()?;
+
+    let y2 = y1.to_owned();
+    if y2 == guess {
+        Ok(format!("YOur guess is correct {y2}"))
+    } else if y2 < guess {
+        Ok(format!("YOur guess is greater"))
+    } else {
+        Ok(format!("YOur guess is lesser"))
+    }
+}
+
+async fn hello_world_handler() -> &'static str {
+    "Hello Selva"
+}
+
+async fn handler(Extension(state): Extension<AppState>) -> String {
+    let y = state.number_to_guess;
+    format!("THE NUMBER IS :: {y}")
+}
+
+async fn guess_handler(Path(user_id): Path<u32>, Extension(state): Extension<AppState>) -> String {
+    let y: u32 = state.number_to_guess;
+
+    if y == user_id {
+        format!("YOur guess is correct {y}")
+    } else {
+        format!("YOur guess is wrong")
+    }
+}
+
+// fn parse_config(path: impl AsRef<Path>) -> Result<Config, Report<ParseConfigError>> {
+//     let path = path.as_ref();
+
+//     // First, we have a base error:
+//     let io_result = fs::read_to_string(path);      // Result<File, std::io::Error>
+
+//     // Convert the base error into a Report:
+//     let io_report = io_result.report();            // Result<File, Report<std::io::Error>>
+
+//     // Change the context, which will eventually return from the function:
+//     let config_report = io_report
+//         .change_context(ParseConfigError::new())?; // Result<File, Report<ParseConfigError>>
+
+//     // ...
+// }
+
+impl<E> From<E> for GuessError2
+where
+    E: Into<Report<GuessError>>,
+{
+    fn from(_err: E) -> Self {
+        Self::default()
+    }
+}
